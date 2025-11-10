da9edfac91cea5a88cad30ffd512a7d1afba7662
Egor1dzeN <egormorozov123450@gmail.com>
2025-11-10 16:22:32 +0300

    add: tests

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -1,12 +1,8 @@
 #include <iostream>
 #include "gtest/gtest.h"
+//#include "gtest/gtest.h"
 
-TEST(HelloTest, BasicAssertions) {
-// Expect two strings not to be equal.
-    EXPECT_STRNE("hello", "world");
-// Expect equality.
-    EXPECT_EQ(7 * 6, 42);
-}
+
 
 int main() {
     testing::InitGoogleTest();
